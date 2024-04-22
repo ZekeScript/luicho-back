1d283de36a263db7b7f75d15e3f923f38e6acab6
unknown <estiga27@gmail.com>
2024-04-21 21:01:14 -0300

    add gitignore and complete package.json

--- source/ProductManager.js
+++ source/ProductManager.js
@@ -1,16 +1,16 @@
 class ProductManager {
-  //iniciar array de productos vacio
-  constructor() {
-    this.products = [];
+  // iniciar array de productos vacio
+  constructor () {
+    this.products = []
   }
 
-  //generar id unico
-  generateUniqueId() {
-    return Math.random().toString(36).slice(2, 11);
+  // generar id unico
+  generateUniqueId () {
+    return Math.random().toString(36).slice(2, 11)
   }
 
-  //agregar producto
-  addProduct(title, description, price, thumbnail, code, stock) {
+  // agregar producto
+  addProduct (title, description, price, thumbnail, code, stock) {
     const product = {
       title,
       description,
@@ -18,68 +18,68 @@ class ProductManager {
       thumbnail,
       code,
       stock,
-      id: this.products.length > 0 ? this.generateUniqueId() : 1,
-    };
-    //evitar carga repetida
+      id: this.products.length > 0 ? this.generateUniqueId() : 1
+    }
+    // evitar carga repetida
     if (this.products.some((product) => product.code === code)) {
-      return `Ya existe un producto con ese codigo`;
+      return 'Ya existe un producto con ese codigo'
     } else {
-      this.products.push(product);
+      this.products.push(product)
     }
   }
 
-  //listar productos
-  getProducts() {
-    return this.products;
+  // listar productos
+  getProducts () {
+    return this.products
   }
 
-  //busqueda x id
-  getProductById(searchedId) {
+  // busqueda x id
+  getProductById (searchedId) {
     return (
       this.products.find((product) => product.id === searchedId) ||
       `no existe producto con el id ${searchedId}`
-    );
+    )
   }
 }
 
-//instancia clase padre
-const prodMgr = new ProductManager();
+// instancia clase padre
+const prodMgr = new ProductManager()
 
-//test array vacio
-console.log(prodMgr.getProducts());
+// test array vacio
+console.log(prodMgr.getProducts())
 
-//test agregar producto
+// test agregar producto
 prodMgr.addProduct(
-  "producto prueba 1",
-  "Este es un producto prueba",
+  'producto prueba 1',
+  'Este es un producto prueba',
   200,
-  "Sin imagen",
-  "abc123",
+  'Sin imagen',
+  'abc123',
   25
-);
-//test de carga repetida
+)
+// test de carga repetida
 prodMgr.addProduct(
-  "producto prueba 1",
-  "Este es un producto prueba",
+  'producto prueba 1',
+  'Este es un producto prueba',
   200,
-  "Sin imagen",
-  "abc123",
+  'Sin imagen',
+  'abc123',
   25
-);
+)
 prodMgr.addProduct(
-  "producto prueba 2",
-  "Este es un producto prueba",
+  'producto prueba 2',
+  'Este es un producto prueba',
   200,
-  "Sin imagen",
-  "def456",
+  'Sin imagen',
+  'def456',
   25
-);
+)
 
-//test listada de productos
-console.log(prodMgr.getProducts());
+// test listada de productos
+console.log(prodMgr.getProducts())
 
-//test busqueda x id; failure path
-console.log(prodMgr.getProductById(0));
+// test busqueda x id; failure path
+console.log(prodMgr.getProductById(0))
 
-//test busqueda x id; success path
-console.log(prodMgr.getProductById(1));
+// test busqueda x id; success path
+console.log(prodMgr.getProductById(1))
