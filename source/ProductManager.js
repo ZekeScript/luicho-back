6c9aa0b97927ee7534a55170cdad80462bd8baf6
unknown <estiga27@gmail.com>
2024-04-25 21:49:17 -0300

    add FS for the rest of the methods

--- source/ProductManager.js
+++ source/ProductManager.js
@@ -1,7 +1,9 @@
+const { existsSync, promises } = require('graceful-fs')
+
 class ProductManager {
   // Inicializa el array de productos vacío
-  constructor () {
-    this.products = []
+  constructor (path) {
+    this.path = path
   }
 
   // Genera un ID único para los productos
@@ -10,20 +12,24 @@ class ProductManager {
   }
 
   // Agrega un producto al array de productos
-  addProduct ({ product }) {
-    const newProduct = this.createProduct({ ...product })
-    // Evita la carga de productos con códigos duplicados
-    if (this.products.some((existingProduct) => existingProduct.code === newProduct.code)) {
-      return 'Ya existe un producto con ese codigo'
-    } else {
-      const updatedProducts = [...this.products, newProduct]
-      this.products = updatedProducts
-      return newProduct
+  async addProduct ({ product }) {
+    try {
+      const newProduct = this.createProduct({ ...product })
+      const products = await this.getProducts()
+      // Evita la carga de productos con códigos duplicados
+      if (products.some((existingProduct) => existingProduct.code === newProduct.code)) {
+        return 'Ya existe un producto con ese codigo'
+      } else {
+        const updatedProducts = [...products, newProduct]
+        await this.writeProducts(updatedProducts)
+      }
+    } catch (error) {
+      console.log(error)
     }
   }
 
   // Crea un objeto producto con los datos de entrada
-  createProduct (title, description, price, thumbnail, code, stock) {
+  createProduct ({ title, description, price, thumbnail, code, stock }) {
     const newProduct = {
       title,
       description,
@@ -37,31 +43,64 @@ class ProductManager {
   }
 
   // Devuelve todos los productos
-  getProducts () {
-    return this.products
+  async getProducts () {
+    try {
+      if (existsSync(this.path)) {
+        const productsData = await promises.readFile(this.path, 'utf8')
+        return JSON.parse(productsData)
+      } else {
+        return []
+      }
+    } catch (error) {
+      console.log(error)
+      return []
+    }
+  }
+
+  async writeProducts (products) {
+    try {
+      await promises.writeFile(this.path, JSON.stringify(products, null, 2))
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   // Busca un producto por su ID
-  getProductById (id) {
-    const product = this.products.find(product => product.id === id)
-    return (!product) ? 'product not found' : product
+  async getProductById (id) {
+    try {
+      const products = await this.getProducts()
+      const product = products.find(product => product.id === id)
+      return product || 'product not found'
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   // Modica un producto por su ID usando los datos de entrada
-  updateProduct (id, updatedFields) {
-    const productIndex = this.products.findIndex(product => product.id === id)
-    if (productIndex === -1) {
-      throw new Error('Product not found')
+  async updateProduct (id, updatedFields) {
+    try {
+      const products = this.getProducts()
+      const productIndex = products.findIndex(product => product.id === id)
+      if (productIndex === -1) {
+        throw new Error('Product not found')
+      }
+      products[productIndex] = { ...products[productIndex], ...updatedFields }
+      return products[productIndex]
+    } catch (error) {
+      console.log(error)
     }
-    this.products[productIndex] = { ...this.products[productIndex], ...updatedFields }
-    return this.products[productIndex]
   }
 
   // Elimina un producto del array de productos usando su ID
-  deleteProduct (id) {
-    const newProducts = this.products.filter(product => product.id !== id)
-    this.products = newProducts
-    return newProducts
+  async deleteProduct (id) {
+    try {
+      const products = this.getProducts()
+      const newProducts = products.filter(product => product.id !== id)
+      await this.writeProducts(newProducts)
+      return newProducts
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 
