845502f5d578f6cd83878f2d0f56924ea71f6b22
unknown <estiga27@gmail.com>
2024-04-30 20:26:47 -0300

    intall nodemon and create server

---
+++ source/products.js
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    name: 'Collar',
+    price: 1000
+  },
+  {
+    name: 'Aros',
+    price: 1000
+  },
+  {
+    name: 'Tobillera',
+    price: 1000
+  },
+  {
+    name: 'Anillos',
+    price: 1000
+  },
+  {
+    name: 'Piercing',
+    price: 1000
+  }
+]
