94f209fdd6f3d3eec961d951e0c2f39e11a3d9eb
unknown <estiga27@gmail.com>
2024-05-01 00:03:43 -0300

    install express

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-import http from 'http'
+import { express } from 'express'
 import { products } from './source/products.js'
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/home') {
-    res.end('home')
-  }
-  console.log(req.url)
-  if (req.url === '/products') {
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(products))
-  }
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json(products)
 })
 
-server.listen(8080, () => console.log('Server ok en puerto 8080'))
+const PORT = 8080
+
+app.listen(PORT, () => console.log(`Server ok${PORT}`))
